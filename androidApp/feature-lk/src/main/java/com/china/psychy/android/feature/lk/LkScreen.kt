0760a6f2ae52db29d28736c88d7d376cec128184
gakalinich <gakalinich@mts.ru>
2024-02-18 21:28:25 +0300

    Добавил фичу ЛК

---
+++ androidApp/feature-lk/src/main/java/com/china/psychy/android/feature/lk/LkScreen.kt
@@ -0,0 +1,10 @@
+package com.china.psychy.android.feature.lk
+
+import androidx.compose.runtime.Composable
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+@Composable
+fun LkScreen() {
+
+}
